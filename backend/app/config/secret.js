ad88deccadab0521190fc975103da9a010577e30
truanv <truanv@vnpt.vn>
2021-12-20 20:30:56 +0700

    add auth

---
+++ backend/app/config/secret.js
@@ -0,0 +1,7 @@
+const secretKey = {
+    jwtSecret: '3duM33t_5ecr3t_ke1'
+};
+
+module.exports =  {
+    secretKey
+}
--- backend/app/db/config.js
+++ backend/app/db/config.js
@@ -1,13 +1,31 @@
+// Connect mongo
 const mongoose = require('mongoose');
 
 const MONGODB_URL = 'mongodb+srv://root:vnpt@cluster0.wabrb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 
 mongoose.connect(MONGODB_URL, { useNewUrlParser: true, useUnifiedTopology: true });
 
-//Get the default connection
-const db = mongoose.connection;
-
-//Bind connection to error event (to get notification of connection errors)
-// db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+const connection = mongoose.connection;
+connection.once('open', () => console.log('MongoDB --  database connection established successfully!'));
+connection.on('error', (err) => {
+	console.log("MongoDB connection error. Please make sure MongoDB is running. " + err);
+	process.exit();
+});
 
 module.exports = mongoose;
+
+
+//conect Redis
+const Redisdb = require('./redis/redisdb');
+
+var db = Redisdb.create({		
+    host: "10.70.123.66",
+    port: "6379"
+});
+
+global.db = db;
+
+db.client.on('error', function () {
+	console.log((new Date()) + " Redis: disconnected");
+	process.exit();
+});
